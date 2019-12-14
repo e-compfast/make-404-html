d6241cc7459b94b318428702a53555ea237ca883
E-compfast Digital Indonesia <58408252+e-compfast@users.noreply.github.com>
2019-12-15 00:33:41 +0700

    Update datatrx.js

--- js/datatrx.js
+++ js/datatrx.js
@@ -1,14 +1,14 @@
- var dataSales = {
+var dataSales = {
         "Erna Wati": {
-            "screenshotURL": "trx/trx-ibu-erna.png",
-            "avatarURL": "https://e-compfast.github.io/trx/bu-erna.jpg",
+            "screenshotURL": "https://e-compfast.github.io/make-404-html/trx/trx-ibu-erna.png",
+            "avatarURL": "https://e-compfast.github.io/make-404-html/trx/bu-erna.jpg",
             "time": "2019-10-14",
             "productName": "Membeli 1 Box Jamu Tetes Labdawara",
             "productURL": "/",
         },
         "Revan": {
-            "screenshotURL": "trx/trx-revan.png",
-            "avatarURL": "https://e-compfast.github.io/trx/bp-revan.jpg",
+            "screenshotURL": "https://e-compfast.github.io/make-404-html/trx/trx-revan.png",
+            "avatarURL": "https://e-compfast.github.io/make-404-html/trx/bp-revan.jpg",
             "time": "2018-08-23",
             "productName": "Menjadi Member PT.Jokam Sembilan Sembilan",
             "productURL": "/",
