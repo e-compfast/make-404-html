84609186f338f46fd9106381171008670bcc0f13
E-compfast Digital Indonesia <58408252+e-compfast@users.noreply.github.com>
2019-12-15 00:01:46 +0700

    Add files via upload

---
+++ js/datatrx.js
@@ -0,0 +1,17 @@
+ var dataSales = {
+        "Erna Wati": {
+            "screenshotURL": "trx/trx-ibu-erna.png",
+            "avatarURL": "https://e-compfast.github.io/trx/bu-erna.jpg",
+            "time": "2019-10-14",
+            "productName": "Membeli 1 Box Jamu Tetes Labdawara",
+            "productURL": "/",
+        },
+        "Revan": {
+            "screenshotURL": "trx/trx-revan.png",
+            "avatarURL": "https://e-compfast.github.io/trx/bp-revan.jpg",
+            "time": "2018-08-23",
+            "productName": "Menjadi Member PT.Jokam Sembilan Sembilan",
+            "productURL": "/",
+        },
+        
+    };
