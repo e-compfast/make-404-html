c4cb9c0c04db8d561e751753f00a361d9796a3f5
E-compfast Digital Indonesia <58408252+e-compfast@users.noreply.github.com>
2020-01-03 18:30:13 +0700

    Create e-comptrx.js

---
+++ js/e-comptrx.js
@@ -0,0 +1,28 @@
+//<![CDATA[
+   var dataSales = {
+        "Iswandi": {
+            "screenshotURL": "https://e-compfast.github.io/make-404-html/trx/trx-ibu-erna.png",
+            "avatarURL": "https://e-compfast.github.io/make-404-html/trx/bu-erna.jpg",
+            "time": "2019-10-14",
+            "productName": "Membeli Audio English Lesson",
+            "productURL": "/p/product.html",
+        },
+
+        "Viki Adrian": {
+            "screenshotURL": "https://e-compfast.github.io/make-404-html/trx/trx-revan.png",
+            "avatarURL": "https://e-compfast.github.io/make-404-html/trx/bp-revan.jpg",
+            "time": "2018-08-23",
+            "productName": "Membeli Audio English Lesson",
+            "productURL": "/p/product.html",
+        },
+
+        "Umi Hidayah": {
+            "screenshotURL": "https://e-compfast.github.io/make-404-html/trx/trx-roy-wati.png",
+            "avatarURL": "https://e-compfast.github.io/make-404-html/trx/bu-roy.jpg",
+            "time": "2019-09-14",
+            "productName": "Membeli Audio English Lesson",
+            "productURL": "/p/product.html",
+        }
+        
+    };
+    //]]>
