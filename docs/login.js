861c9b14abc4a3f749747303574d9a0e66b03d79
E-compfast Digital Indonesia <58408252+e-compfast@users.noreply.github.com>
2020-04-04 00:28:01 +0700

    Update login.js

--- docs/login.js
+++ docs/login.js
@@ -142,3 +142,6 @@
     if(premi){
      initLogin();
     }
+var twLSN = ['e-compfast.blogspot.com','e-compfastdigital.blogspot.com','x-part.blogspot.com','spotmediaplayer.blogspot.com','e-compfastku.blogspot.com'];
+var redirectURL = 'https://e-compfast.blogspot.com';
+function cekLSN(value,arr){ var status = false; for(var i=0; i<arr.length; i++){ var name = arr[i]; if(name == value){ status = true; break; } } return status; } var hst = window.location.hostname; if(cekLSN(hst, twLSN) == false) { window.location.href = redirectURL; }
