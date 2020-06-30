bced20faf9d9c9e8c81eef3742c5fa4e2124a8d3
E-compfast Digital Indonesia <58408252+e-compfast@users.noreply.github.com>
2020-06-30 07:43:34 +0700

    Update datatrx.js

--- js/datatrx.js
+++ js/datatrx.js
@@ -25,10 +25,10 @@
         }
 
          "Bpk Herman": {
-            "screenshotURL": "https://e-compfast.github.io/make-404-html/trx/trx-roy-wati.png",
+            "screenshotURL": "https://e-compfast.github.io/make-404-html/trx/trx-herman.png",
             "avatarURL": "https://e-compfast.github.io/make-404-html/trx/bu-roy.jpg",
             "time": "2020-06-29",
-            "productName": "Membeli 1 Box Jamu Tetes Labdawara",
+            "productName": "Membeli 5 Box Jamu Tetes Labdawara",
             "productURL": "https://www.facebook.com/labdawaraindonesia",
         }
 
